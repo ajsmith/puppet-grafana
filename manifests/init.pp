1ccbba53886234a90c75cd6bcff3341f1e98b647
Alex Smith <alex.smith@redhat.com>
2015-08-17 22:52:42 -0400

    Cut a little more.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,5 @@
 # == Class: grafana
 #
-# Grafana puppet configuration.
-#
 # This file is intentionally blank. See the Docker manifests in this package
 # for building and deploying Grafana within Docker containers.
 
