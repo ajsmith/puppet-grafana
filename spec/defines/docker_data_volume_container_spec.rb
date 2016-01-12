e107b71d524f0531e85d71b96472e11ad3b513b4
Alex Smith <alex.smith@redhat.com>
2016-01-12 17:41:11 -0500

    Add rspec test for grafana::docker::data_volume_container.

---
+++ spec/defines/docker_data_volume_container_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper.rb'
+
+describe 'grafana::docker::data_volume_container' do
+  context 'with defaults for all parameters' do
+    let(:title) { 'grafana-data' }
+    it { should compile }
+    it { should contain_docker_systemd__data_volume_container('grafana-data').with({ 'image' => 'grafana' }) }
+  end
+end
