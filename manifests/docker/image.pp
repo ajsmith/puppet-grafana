67b52190130c7358ab259b7c208b041a68b858d5
Alex Smith <alex.smith@redhat.com>
2016-02-10 13:57:22 -0500

    Document image parameters.

--- manifests/docker/image.pp
+++ manifests/docker/image.pp
@@ -1,6 +1,24 @@
 # == Class: grafana::docker::image
 #
 # Configure a host to build the Grafana docker image.
+#
+# === Parameters
+#
+# [docker_dir]
+#
+#   The path to the directory which should hold the Grafana Docker resources.
+#
+# [vcs_revision]
+#
+#   The revision of Grafana Docker that should be used. Usually this should be
+#   left to the default.
+#
+# [image_config_cls]
+#
+#   The name of the class which customizes the configuration of the Grafana
+#   Docker image. A different class can be provided here to further customize
+#   the Grafana image.
+#
 
 class grafana::docker::image (
   $docker_dir       = '/opt/docker-grafana',
